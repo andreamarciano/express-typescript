0af5a58f07cdb44170b4eb5dd98e4520fa39d4ea
andreamarciano <andrea.marciano.66@gmail.com>
2025-06-14 09:57:47 +0200

    public pages, routing

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,45 @@
 import express, { Request, Response } from "express";
+import path from "path"; // os compatibility
 
 const app = express();
-const PORT = 3000;
+const PORT: number = 3000;
 
+// 200 OK - 304 Not Modified (Cache) - 404 Not Found
+app.use((req, res, next) => {
+  res.on("finish", () => {
+    console.log(`${req.method} ${req.originalUrl} - ${res.statusCode}`);
+  });
+  next();
+});
+
+// Serves all files in the "public" folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// Home
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello from TypeScript + Express!");
+  res.sendFile("homepage.html", { root: path.join(__dirname, "public") });
+});
+
+// People
+app.get("/people", (req: Request, res: Response) => {
+  res.sendFile("people.html", { root: path.join(__dirname, "public") });
+});
+
+// About
+app.get("/about", (req: Request, res: Response) => {
+  res.sendFile("about.html", { root: path.join(__dirname, "public") });
+});
+
+// Contacts
+app.get("/contact", (req: Request, res: Response) => {
+  res.sendFile("contact.html", { root: path.join(__dirname, "public") });
+});
+
+// Undefined Routes (404)
+app.use((req: Request, res: Response) => {
+  res
+    .status(404)
+    .sendFile("404.html", { root: path.join(__dirname, "public") });
 });
 
 app.listen(PORT, () => {
